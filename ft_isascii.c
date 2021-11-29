24773d7692734b4728a59bd9a36127773d8c12ba
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-11-29 02:34:30 +0100

    Update ft_isascii.c

--- ft_isascii.c
+++ ft_isascii.c
@@ -10,6 +10,47 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+  NAME
+  ****
+  ft_isascii ---> is a ascii character
+
+  SYNOPSIS
+  ***********
+  #include <ctype.h>
+  int	isascii(int c);
+  #include "libft.h"
+  int	ft_isascii(int c);
+
+  DESCRIPTION
+  ***********
+  The function ft_isascii() mimics the behavior of the isascii() function from
+  ctype C library. Both functions check if c is a 7-bit unsigned char value
+  that fits into the ASCII character set.
+
+  PARAMETERS
+  **********
+  c ---> The character to be checked.
+
+  RETURN VALUE
+  ************
+  - Non-zero value if c is a digit or a letter.
+  - Zero value if not.
+
+  NOTES	
+  *****
+  isascii() is a BSD extension and is also an SVr4 extension.
+  POSIX.1-2008 marks isascii() as obsolete, noting that it cannot be used portably
+  in a localized application.
+*/
+/*
+  RETURN A CONDITION
+  ******************
+  When we return a condition, the computer will check if the condition is true or
+  false. If it is true it will return 1(boolean true) and if it is false it will
+  return 0(boolean false).
+*/
+
 int	ft_isascii(int c)
 {
 	return (c >= 0 && c <= 127);
