3df742414d98454859f4801c1793f98691001faf
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-11-29 02:26:24 +0100

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -12,6 +12,33 @@
 
 #include "libft.h"
 
+/*
+  NAME
+  ****
+  ft_isalnum ---> is an alphanumeric character
+
+  SYNOPSIS
+  ***********
+  #include <ctype.h>
+  int	isalnum(int c);
+  #include "libft.h"
+  int	ft_isalnum(int c);
+
+  DESCRIPTION
+  ***********
+  The function ft_isalnum() mimics the behavior of the isalnum() function from
+  ctype C library. Both functions check for an alphanumeric character.
+
+  PARAMETERS
+  **********
+  c ---> The character to be checked.
+
+  RETURN VALUE
+  ************
+  - Non-zero value if c is a digit or a letter.
+  - Zero value if not.
+*/
+
 int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
