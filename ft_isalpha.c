111fbfbe0e0a455dffa02dd63526cc8a0168c0de
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-11-29 02:33:11 +0100

    Update ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -10,6 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+  NAME
+  ****
+  ft_isalpha ---> is an alphabetic character
+
+  SYNOPSIS
+  ***********
+  #include <ctype.h>
+  int	isalpha(int c);
+  #include "libft.h"
+  int	ft_isalpha(int c);
+
+  DESCRIPTION
+  ***********
+  The function ft_isalpha() mimics the behavior of the isalpha() function from
+  ctype C library. Both functions check if c is an alphabetic character.
+
+  PARAMETERS
+  **********
+  c ---> The character to be checked.
+
+  RETURN VALUE
+  ************
+  - Non-zero value if c is a letter.
+  - Zero value if not.
+*/
+/*
+  RETURN A CONDITION
+  ******************
+  When we return a condition, the computer will check if the condition is true or
+  false. If it is true it will return 1(boolean true) and if it is false it will
+  return 0(boolean false).
+*/
+
 int	ft_isalpha(int c)
 {
 	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
