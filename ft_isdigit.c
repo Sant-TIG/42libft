5262907e8f8a41a12ec45e2ac1f57e7cd0439f3b
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2021-11-29 02:35:23 +0100

    Update ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -10,6 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+  NAME
+  ****
+  ft_isdigit ---> is a digit character
+
+  SYNOPSIS
+  ***********
+  #include <ctype.h>
+  int	isdigit(int c);
+  #include "libft.h"
+  int	ft_isdigit(int c);
+
+  DESCRIPTION
+  ***********
+  The function ft_isdigit() mimics the behavior of the isdigit() function from
+  ctype C library. Both functions check whether c is digit (0 through 9) or not.
+
+  PARAMETERS
+  **********
+  c ---> The character to be checked.
+
+  RETURN VALUE
+  ************
+  - Non-zero value if c is a digit.
+  - Zero value if not.
+*/
+/*
+  RETURN A CONDITION
+  ******************
+  When we return a condition, the computer will check if the condition is true or
+  false. If it is true it will return 1(boolean true) and if it is false it will
+  return 0(boolean false).
+*/
+
 int	ft_isdigit(int c)
 {
 	return (c >= 48 && c <= 57);
