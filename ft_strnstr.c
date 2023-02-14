46839a7932a532361a4cabb6521562ef2ee1a4fb
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2023-02-14 23:44:59 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,6 +12,38 @@
 
 #include "libft.h"
 
+/*
+  NAME
+  ****
+  ft_strnstr ---> search for a substring in a string to at most n bytes.
+  
+  SYNOPSIS
+  ********
+  #include "mylibft.h"
+  char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
+  
+  PARAMETERS
+  **********
+  haystack ---> the string where to find the substring.
+  needle   ---> the substring to find.
+  n        ---> the number of characters to be checked(at most).
+  
+  DESCRIPTION
+  ***********
+  The ft_strnstr() function searches for the first ocurrence of the substring
+  'needle' in the string 'haystack', where no more than n bytes are searched.
+  If it finds the entire secuence of characters specified in 'needle' within
+  the limit marked by n, it returns a pointer to the beginning of the first
+  occurrence of the substring.
+  
+  RETURN VALUE
+  ************
+  - If 'needle' is in 'haystack' searching at most n bytes, returns a pointer
+    to the beginning of its first occurrence.
+  - If not, returns NULL.
+  - If 'needle is empty string, returns haystack.
+*/
+
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
