079c3b50f4b61099c996f2e9efc522c0a96d7f26
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2023-02-14 23:11:15 +0100

    Update ft_tolower.c

--- ft_tolower.c
+++ ft_tolower.c
@@ -10,9 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+  NAME
+  ****
+  ft_tolower ---> convert char to lowercase
+  
+  SYNOPSIS
+  ********
+  #include "convert.h"
+  int	ft_tolower(int c);
+  
+  PARAMETERS
+  **********
+  c --> The character to convert.
+  
+  DESCRIPTION
+  ***********
+  The ft_tolower() function uses the ft_isupper() function to check if 'c'
+  is alphabetic and uppercase. If it is the case, converts it to lowercase and
+  if it not, it does not modify it.
+  
+  RETURN VALUE
+  ************
+  - The character converted to lowercase.
+  - If it is not possible to convert, the original character.
+*/
+
+#include "libft.h"
+
 int	ft_tolower(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (ft_isupper(c))
 		return (c + 32);
 	return (c);
 }
