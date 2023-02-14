2fe1c47f76cb0ed122af4f67a682c2206a458e3c
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2023-02-14 23:44:04 +0100

    Update ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -12,15 +12,44 @@
 
 #include "libft.h"
 
+/*
+  NAME
+  ****
+  ft_strrchr ---> reverse order search for a character in a string.
+  
+  SYNOPSIS
+  ********
+  #include "libft.h"
+  char	*ft_strrchr(const char *str, int c);
+  
+  PARAMETERS
+  **********
+  str ---> the string where to look for the character.
+  c   ---> the character to find.
+  
+  DESCRIPTION
+  ***********
+  The ft_strrchr() function searches for the last ocurrence of the character 'c'
+  in he string pointed to by 'str'.
+  
+  If it finds it, it returns a pointer to it so that we would have acces to the
+  rest of the string starting from that character.
+  
+  RETURN VALUE
+  ************
+  - If c is in str, returns a pointer to its last occurrence.
+  - If not, returns NULL.
+*/
+
 char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
 
-	i = ft_strlen(s) + 1;
+	i = ft_strlen(s);
 	while (i--)
 	{
-		if (((char *)s)[i] == (char)c)
-			return ((char *)&s[i]);
+		if (*(s + i) == (char)c)
+			return (&(s + i);
 	}
 	return (NULL);
 }
