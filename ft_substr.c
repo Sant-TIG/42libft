cfb6aed030675dad5dc479dbc63c4ad67757b881
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2023-02-14 23:19:03 +0100

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -9,21 +9,62 @@
 /*   Updated: 2021/08/23 18:37:33 by sperez-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*dest;
+/*
+NAME
+****
+ft_substr ---> extract a substring from a string
 
-	if (!s)
-		return (NULL);
-	if (start > ft_strlen(s))
+SYNOPSIS
+********
+#include "strings.h"
+char	*ft_substr(const char *str, unsgined int start, size_t len);
+
+PARAMETERS
+**********
+str   ---> the string where to extract the substring
+start ---> the index of the string where the substring starts
+len   ---> the maximum length of the substring
+
+DESCRIPTION
+***********
+The ft_substr() function extracts a substring from a string. This substring
+will start from the 'start' index of the string and will have a maximum length
+of 'len' bytes. However, there are two things to keep in mind:
+
+- The value of 'start' must not be greater than the length of 'str'. In that
+case, it returns a string consisiting of a single null character.
+
+- The number of bytes to be copied is always limited to a maximum equal to the
+number of bytes that can be copied from the original string from the 'start'
+position to the end of the string.
+
+RETURN VALUE
+************
+- A pointer to the substring.
+- If 'start' is greater than the length of 'str', returns a string consisting of
+a single null character.
+- If there is an error allocating the memory space or if 'str' is NULL, it returns NULL.
+*/
+
+char	*ft_substr(const char *str, unsigned int start, size_t len)
+{
+	size_t	new_len;
+	char	*dst;
+
+	if (!str)
+		return(NULL);
+	if (start > ft_strlen(str))
 		return (ft_strdup(""));
-	if (ft_strlen(s) < len)
-		len = ft_strlen(s);
-	dest = (char *)malloc(sizeof(char) * len + 1);
-	if (!dest)
+	str = str + start;
+	new_len = ft_strlen (str);
+	if (len > new_len)
+		len = new_len;
+	dst = (char *)malloc(sizeof(char) * len + 1);
+	if (!dst)
 		return (NULL);
-	ft_strlcpy(dest, &s[start], len + 1);
-	return (dest);
+	ft_strlcpy(dst, str, len + 1);
+	return (dst);	
 }
