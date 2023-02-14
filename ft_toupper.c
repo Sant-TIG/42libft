742eb12b775a82ed22077a684aaa360f6e3180a4
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2023-02-14 23:09:56 +0100

    Update ft_toupper.c

--- ft_toupper.c
+++ ft_toupper.c
@@ -10,9 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+  NAME
+  ****
+  ft_toupper ---> convert char to uppercase
+  
+  SYNOPSIS
+  ********
+  #include "libft.h"
+  int	ft_toupper(int c);
+  
+  PARAMETERS
+  **********
+  c --> The character to convert.
+  
+  DESCRIPTION
+  ***********
+  The ft_toupper() function uses the ft_islower() function to check if 'c'
+  is alphabetic and lowercase. If it is the case, converts it to uppercase and
+  if it not, it does not modify it.
+  
+  RETURN VALUE
+  ************
+  - The character converted to uppercase.
+  - If it is not possible to convert, the original character.
+*/
+
+#include "libft.h"
+
 int	ft_toupper(int c)
 {
-	if (c >= 97 && c <= 122)
+	if (ft_islower(c))
 		return (c - 32);
 	return (c);
 }
