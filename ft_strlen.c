96f42b62ab0637f06d926c794abff59663024400
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2023-02-14 23:47:58 +0100

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -12,6 +12,31 @@
 
 #include "libft.h"
 
+/*
+NAME
+****
+ft_strlen ---> calculate the length of a string
+
+SYNOPSIS
+********
+#include "mylibft.h"
+size_t	ft_strlen(const char *str);
+
+PARAMETERS
+**********
+str ---> the string whose length is to be found
+
+DESCRIPTION
+***********
+The ft_strlen() function calculates the length of 'str', excluiding the 
+terminating null byte '\0'. If the content of 'str' if NULL, returns zero.
+
+RETURN VALUE
+************
+- The number of bytes in 'str'.
+- If 'str' is NULL, returns zero.
+*/
+
 size_t	ft_strlen(const char *s)
 {
 	size_t	i;
